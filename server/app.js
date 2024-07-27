04d07bbe483c744d975ac1d720eedda1b0e505b7
yuhari7 <yuhariermy@gmail.com>
2024-07-28 04:50:47 +0700

    Testing Development & Production Environtment

--- server/app.js
+++ server/app.js
@@ -1,13 +1,28 @@
-import morgan from 'morgan';
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+
 import express from 'express';
+import morgan from 'morgan';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import TourRouter from './routes/tourRoutes.js';
 import UserRouter from './routes/userRoutes.js';
 
 const app = express();
 
 // 1) Middleware
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 import app from './app.js';
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
