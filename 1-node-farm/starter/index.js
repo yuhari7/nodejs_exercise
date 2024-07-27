bf4849fd945be8d89b102930de5969fed9f99ca9
yuhari7 <yuhariermy@gmail.com>
2024-07-28 00:49:35 +0700

    NodeJS | Streams

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,24 +1,36 @@
 const fs = require('fs')
+const http = require('http')
 
-// ============BLOCKING WAYS============
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(textIn);
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt', textOut)
-console.log('File written!');
+// ============CLIENT=============
 
-// =============NON-BLOCKING ASYNCHRONOUS WAY===============
+// // ============BLOCKING WAYS============
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File written!');
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
+// // =============NON-BLOCKING ASYNCHRONOUS WAY===============
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written!');
-            })
-        })
-    })
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written!');
+//             })
+//         })
+//     })
+// })
+// console.log('Will read file!');
+
+// ============SERVER=============
+const server = http.createServer((req,res) => {
+    res.end('Hello from the server!')
 })
-console.log('Will read file!');
+
+server.listen(8080, '127.0.0.1', () => {
+    console.log('Server is listening on port 8080');
+});
